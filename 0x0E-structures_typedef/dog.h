cbdd3c01a7ff61f25b5332eea83537f150fd466a
Andre Ozeghe <andreozeghe@gmail.com>
2023-09-11 21:30:55 +0100

    pushing

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
 /*
  * @struct dog: a dog's basic info
  * @name: name of dog
@@ -28,5 +19,15 @@ struct dog
 	float age;
 	char *owner;
 }
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 
 #endif
