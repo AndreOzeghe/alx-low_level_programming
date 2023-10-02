95d766f5118d850e0adffe6109e90562525f3e0c
Andre Ozeghe <andreozeghe@gmail.com>
2023-10-02 20:20:03 +0100

    allll

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,10 +14,11 @@ void close_file(int fd);
 char *create_buffer(char *file)
 {
 	char *buffer;
+
 	buffer = malloc(sizeof(char) * 1024);
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
 	return (buffer);
